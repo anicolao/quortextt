2053158ce28aa25b7328ac8448e3af02db3a37e5
Alex Nicolaou <anicolao@gmail.com>
2025-06-18 19:26:58 -0400

    First cut of drawing the board

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use crate::game::*;
-use egui::{Sense, Stroke};
+use egui::{Pos2, Sense, Stroke, Vec2};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -42,6 +42,189 @@ impl TemplateApp {
 
         Default::default()
     }
+
+    fn draw_flow(
+        center: egui::Pos2,
+        painter: &egui::Painter,
+        hexagon: &Vec<Pos2>,
+        sp: usize,
+        ep: usize,
+        color: egui::Color32,
+        thickness: f32,
+    ) {
+        let start = (hexagon[sp] + hexagon[(sp + 1) % 6].to_vec2()) * 0.5;
+        let end = (hexagon[ep] + hexagon[(ep + 1) % 6].to_vec2()) * 0.5;
+        let cp1 = (center + start.to_vec2()) * 0.5;
+        let cp2 = (center + end.to_vec2()) * 0.5;
+        painter.add(egui::epaint::CubicBezierShape {
+            points: [start, cp1, cp2, end],
+            stroke: Stroke::new(thickness, color).into(),
+            fill: egui::Color32::TRANSPARENT,
+            closed: false,
+        });
+    }
+    fn draw_inverted_hex(
+        bounding_box: egui::Rect,
+        center: egui::Pos2,
+        hexagon_radius: f32,
+        painter: &egui::Painter,
+        fill: egui::Color32,
+        border: Stroke,
+        rotate: f32,
+    ) {
+        let hexagon = (0..6)
+            .map(|i| {
+                let angle = i as f32 * std::f32::consts::PI / 3.0
+                    + 3.0 * std::f32::consts::PI / 6.0
+                    + rotate;
+                Pos2::new(
+                    center.x + hexagon_radius * angle.cos(),
+                    center.y + hexagon_radius * angle.sin(),
+                )
+            })
+            .collect::<Vec<_>>();
+        for i in 0..6 {
+            let start = hexagon[i];
+            let end = hexagon[(i + 1) % 6];
+            if start.y != end.y && start.x < center.x {
+                let triangle = [
+                    Pos2::new(bounding_box.left(), start.y),
+                    start,
+                    end,
+                    Pos2::new(bounding_box.left(), end.y),
+                ];
+                painter.add(egui::Shape::convex_polygon(triangle.to_vec(), fill, border));
+            }
+            if start.y != end.y && start.x > center.x {
+                let triangle = [
+                    Pos2::new(bounding_box.right(), start.y),
+                    start,
+                    end,
+                    Pos2::new(bounding_box.right(), end.y),
+                ];
+                painter.add(egui::Shape::convex_polygon(triangle.to_vec(), fill, border));
+            }
+            if (start.y - end.y).abs() <= 1.0 {
+                if start.y < center.y {
+                    let triangle = [
+                        Pos2::new(bounding_box.left(), bounding_box.top()),
+                        Pos2::new(bounding_box.right(), bounding_box.top()),
+                        Pos2::new(bounding_box.right(), start.y),
+                        Pos2::new(bounding_box.left(), start.y),
+                    ];
+                    painter.add(egui::Shape::convex_polygon(triangle.to_vec(), fill, border));
+                } else {
+                    let triangle = [
+                        Pos2::new(bounding_box.left(), bounding_box.bottom()),
+                        Pos2::new(bounding_box.left(), start.y),
+                        Pos2::new(bounding_box.right(), start.y),
+                        Pos2::new(bounding_box.right(), bounding_box.bottom()),
+                    ];
+                    painter.add(egui::Shape::convex_polygon(triangle.to_vec(), fill, border));
+                }
+            }
+        }
+    }
+    fn draw_empty_hex(
+        center: egui::Pos2,
+        hexagon_radius: f32,
+        painter: &egui::Painter,
+        fill: egui::Color32,
+        border: Stroke,
+        rotate: f32,
+    ) {
+        let hexagon = (0..6)
+            .map(|i| {
+                let angle = i as f32 * std::f32::consts::PI / 3.0
+                    + 3.0 * std::f32::consts::PI / 6.0
+                    + rotate;
+                Pos2::new(
+                    center.x + hexagon_radius * angle.cos(),
+                    center.y + hexagon_radius * angle.sin(),
+                )
+            })
+            .collect::<Vec<_>>();
+        painter.add(egui::Shape::convex_polygon(hexagon.clone(), fill, border));
+    }
+    fn draw_hex(center: egui::Pos2, hexagon_radius: f32, painter: &egui::Painter) {
+        let hexagon = (0..6)
+            .map(|i| {
+                let angle =
+                    i as f32 * std::f32::consts::PI / 3.0 + 3.0 * std::f32::consts::PI / 6.0;
+                Pos2::new(
+                    center.x + hexagon_radius * angle.cos(),
+                    center.y + hexagon_radius * angle.sin(),
+                )
+            })
+            .collect::<Vec<_>>();
+        painter.add(egui::Shape::convex_polygon(
+            hexagon.clone(),
+            egui::Color32::from_rgb(0x33, 0x33, 0x33),
+            Stroke::new(2.0, egui::Color32::from_rgb(0xAA, 0xAA, 0xAA)),
+        ));
+        let thickness = 8.0 / 35.0 * hexagon_radius;
+        Self::draw_flow(
+            center,
+            painter,
+            &hexagon,
+            0,
+            2,
+            egui::Color32::from_rgb(0xAA, 0xAA, 0xAA),
+            thickness,
+        );
+        Self::draw_flow(
+            center,
+            painter,
+            &hexagon,
+            3,
+            5,
+            egui::Color32::from_rgb(0xAA, 0xAA, 0xAA),
+            thickness,
+        );
+        Self::draw_flow(
+            center,
+            painter,
+            &hexagon,
+            1,
+            4,
+            egui::Color32::from_rgb(0xAA, 0xAA, 0xAA),
+            thickness,
+        );
+        Self::draw_flow(
+            center,
+            painter,
+            &hexagon,
+            0,
+            2,
+            egui::Color32::from_rgb(0xFF, 0x00, 0x00),
+            thickness,
+        );
+        Self::draw_flow(
+            center,
+            painter,
+            &hexagon,
+            3,
+            5,
+            egui::Color32::from_rgb(0x00, 0x00, 0xFF),
+            thickness,
+        );
+        /*
+                Self::draw_flow(
+                    center,
+                    painter,
+                    &hexagon,
+                    1,
+                    4,
+                    egui::Color32::from_rgb(0x00, 0xFF, 0x00),
+                    thickness,
+                );
+        */
+        painter.add(egui::Shape::convex_polygon(
+            hexagon.clone(),
+            egui::Color32::TRANSPARENT,
+            Stroke::new(2.0, egui::Color32::from_rgb(0xAA, 0xAA, 0xAA)),
+        ));
+    }
 }
 
 impl eframe::App for TemplateApp {
@@ -79,9 +262,79 @@ impl eframe::App for TemplateApp {
             let (window, response) =
                 ui.allocate_exact_size(bounding_box, Sense::union(Sense::click(), Sense::hover()));
             let painter = ui.painter();
-            painter.line_segment(
-                [window.min, window.max],
-                Stroke::new(2.0, egui::Color32::from_rgb(0xFF, 0x00, 0x00)),
+
+            let hexagon_radius = 40.0;
+            let centerOffset = hexagon_radius * 0.9;
+            // horizontal vector that moves one tile width to the right
+            let right = Vec2::new(2.0 * centerOffset, 0.0);
+            // diagonal vector that moves one hex up and to the right
+            let up = Vec2::new(
+                2.0 * centerOffset * 0.5,
+                -2.0 * centerOffset * 0.86602540378,
+            );
+            let rup = Vec2::new(-2.0 * centerOffset, 0.0);
+            let bgcolor = egui::Color32::from_rgb(0xFE, 0xFE, 0xF0);
+            let bgcolors = Stroke::new(2.0, egui::Color32::from_rgb(0xFE, 0xFE, 0xF0));
+            let fill = egui::Color32::from_rgb(0xFE, 0xFE, 0xF0);
+            let border = Stroke::new(2.0, egui::Color32::from_rgb(0xAA, 0xAA, 0xAA));
+            Self::draw_empty_hex(
+                window.center(),
+                hexagon_radius * 7.5,
+                painter,
+                egui::Color32::from_rgb(0x33, 0x33, 0x33),
+                border,
+                std::f32::consts::PI / 6.0,
+            );
+            for i in -1..8 {
+                for j in -1..8 {
+                    if i < 0 || j < 0 || i == 7 || j == 7 {
+                        if j == -1 && i > 0 && i <= 4 {
+                            // set the clipping path to the enclosing hexagon
+                            // player 0
+                            let fcolor = egui::Color32::from_rgb(0xFF, 0, 0);
+                            let bcolor = Stroke::new(3.0, egui::Color32::from_rgb(0xFF, 0, 0));
+                            Self::draw_empty_hex(
+                                window.center() + up * -3.0 + right * i as f32 + up * j as f32,
+                                hexagon_radius,
+                                painter,
+                                fcolor,
+                                bcolor,
+                                0.0,
+                            );
+                        }
+                        continue;
+                    }
+                    if (j > 3 && i < j - 3) || (j < 3 && i > 3 + j) {
+                        continue; // skip the empty hexagons
+                    }
+                    let pos = window.center()
+                        + up * -3.0
+                        + right * i as f32
+                        + up * j as f32
+                        + rup * j as f32;
+                    Self::draw_empty_hex(pos, hexagon_radius, painter, fill, border, 0.0);
+                    if (i + j) % 2 == 0 {
+                        Self::draw_hex(pos, hexagon_radius, painter);
+                    }
+                    //Self::draw_hex(pos, hexagon_radius, painter);
+                }
+            }
+            Self::draw_inverted_hex(
+                window,
+                window.center(),
+                hexagon_radius * 7.5,
+                painter,
+                bgcolor,
+                bgcolors,
+                std::f32::consts::PI / 6.0,
+            );
+            Self::draw_empty_hex(
+                window.center(),
+                hexagon_radius * 7.5,
+                painter,
+                egui::Color32::TRANSPARENT,
+                border,
+                std::f32::consts::PI / 6.0,
             );
         });
     }
